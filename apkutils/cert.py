acfd35e089eef7d09122ff9d964998c3ce175aa5
Chunlin Zhang <zhangchunlin@gmail.com>
2018-03-29 17:05:37 +0800

    add crypto.FILETYPE_PEM support to Certificate class

--- apkutils/cert.py
+++ apkutils/cert.py
@@ -8,27 +8,32 @@ from OpenSSL.crypto import _lib, _ffi, X509
 
 class Certificate:
 
-    def __init__(self, buff):
+    def __init__(self, buff, ftype=crypto.FILETYPE_ASN1):
         self.content = []
+        self.ftype = ftype
         self._parse(buff)
 
     def get(self):
         return self.content
 
     def _parse(self, buff):
-        pkcs7 = crypto.load_pkcs7_data(crypto.FILETYPE_ASN1, buff)
-
-        certs_stack = _ffi.NULL
-        if pkcs7.type_is_signed():
-            certs_stack = pkcs7._pkcs7.d.sign.cert
-        elif pkcs7.type_is_signedAndEnveloped():
-            certs_stack = pkcs7._pkcs7.d.signed_and_enveloped.cert
-
         pycerts = []
 
-        for i in range(_lib.sk_X509_num(certs_stack)):
-            tmp = _lib.X509_dup(_lib.sk_X509_value(certs_stack, i))
-            pycert = X509._from_raw_x509_ptr(tmp)
+        if self.ftype == crypto.FILETYPE_ASN1:
+            pkcs7 = crypto.load_pkcs7_data(self.ftype, buff)
+
+            certs_stack = _ffi.NULL
+            if pkcs7.type_is_signed():
+                certs_stack = pkcs7._pkcs7.d.sign.cert
+            elif pkcs7.type_is_signedAndEnveloped():
+                certs_stack = pkcs7._pkcs7.d.signed_and_enveloped.cert
+
+            for i in range(_lib.sk_X509_num(certs_stack)):
+                tmp = _lib.X509_dup(_lib.sk_X509_value(certs_stack, i))
+                pycert = X509._from_raw_x509_ptr(tmp)
+                pycerts.append(pycert)
+        elif self.ftype == crypto.FILETYPE_PEM:
+            pycert = crypto.load_certificate(self.ftype,buff)
             pycerts.append(pycert)
 
         if not pycerts:
@@ -45,7 +50,6 @@ class Certificate:
                 emailAddress=o.emailAddress)
 
         for cert in pycerts:
-
             self.content.append(
                 dict(issuer=X509Name2dict(cert.get_issuer()),
                     serial_number=cert.get_serial_number(),
